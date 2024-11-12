9b2df734e8d3f88ca6a1369daaade8ecdc9c4caa
Gunjan-pandey85340 <gunjan.pandey_cs22@gla.ac.in>
2024-11-12 09:52:02 +0530

    conversions

---
+++ 01_Basics/03_conversionOpr.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+let score1 = null
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1;false =>0
+
+let islogged = ""
+let booleanisloggedin = Boolean(islogged)
+console.log(booleanisloggedin);
+
+// 1 = > true
+// 0=> false
+//"gunj" => true
+//"" => false
+
+let someNum = 33
+let StringNum = String(someNum)
+console.log(typeof StringNum); 
