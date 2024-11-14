e90f0609821ed0f4d0c31918023136c493f3d112
Gunjan-pandey85340 <gunjan.pandey_cs22@gla.ac.in>
2024-11-14 09:43:53 +0530

    array part01

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,45 @@
+// array
+
+// const arr = [0 , 1, 2, 3, 4,5]
+// const myH = ["ram","sita"]
+const myarr = new Array(1,2,3,4)
+console.log(myarr[0]);
+
+//Array methods
+// myarr.push(6) // add
+// console.log(myarr);
+// myarr.push(7);
+// console.log(myarr);
+// myarr.pop() // remove
+
+// myarr.unshift(9)
+// myarr.shift()
+
+// console.log(myarr.includes(9));
+
+// console.log(myarr.indexOf(9));//-1
+// const newArr = myarr.join()
+// console.log(myarr); 
+// console.log(typeof newArr);
+
+//slice,splice
+
+console.log("A",myarr); 
+const myn1 = myarr.slice(1,3)
+console.log(myn1);
+
+console.log("B",myarr);  
+const myn2 = myarr.splice(1,3)
+console.log("C",myarr);
+
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
